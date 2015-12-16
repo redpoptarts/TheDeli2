a9bfd8b593509e4583ae9cd06ebad5b85d0ed109
Caleb Morrell <redpoptarts@gmail.com>
2015-12-15 17:48:59 -0800

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thedeliApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thedeliApp
+ */
+angular.module('thedeliApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
